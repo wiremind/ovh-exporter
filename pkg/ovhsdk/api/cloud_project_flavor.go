a0c5ba5825ce7b74b992d314a72ab1d3dae05a3d
Maxime Leroy <19607336+maxime1907@users.noreply.github.com>
2024-09-23 15:29:08 +0200

    feat(serve): billing: handle multiple project ids (#7)

--- pkg/ovhsdk/api/cloud_project_flavor.go
+++ pkg/ovhsdk/api/cloud_project_flavor.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/ovh/go-ovh/ovh"
 	"github.com/wiremind/ovh-exporter/pkg/ovhsdk/models"
 )
@@ -12,6 +14,26 @@ func FindFlavorByID(flavors []models.Flavor, flavorID string) *models.Flavor {
 		}
 	}
 	return nil
+}
+
+func GetCloudProjectFlavorsPerInstances(ovhClient *ovh.Client, projectID string, projectInstances []models.InstanceSummary) ([]models.Flavor, error) {
+	var flavors []models.Flavor
+	var apierr error = nil
+	for _, instance := range projectInstances {
+		if FindFlavorByID(flavors, instance.FlavorID) == nil {
+			flavor, err := GetCloudProjectFlavor(ovhClient, projectID, instance.FlavorID)
+			if err != nil {
+				if apierr == nil {
+					apierr = fmt.Errorf("Failed to retrieve flavor: %v", err)
+				} else {
+					apierr = fmt.Errorf("%s; %v", apierr, err)
+				}
+			} else {
+				flavors = append(flavors, flavor)
+			}
+		}
+	}
+	return flavors, apierr
 }
 
 func GetCloudProjectFlavor(client *ovh.Client, projectID string, flavorID string) (models.Flavor, error) {
--- pkg/ovhsdk/api/cloud_project_instance_billing.go
+++ pkg/ovhsdk/api/cloud_project_instance_billing.go
@@ -6,35 +6,27 @@ import (
 )
 
 func GetCloudProjectInstances(client *ovh.Client, projectID string) ([]models.InstanceSummary, error) {
-	// Define the response structure for the instance billing
 	var instances []models.InstanceSummary
 
-	// Build the API endpoint
 	endpoint := "/cloud/project/" + projectID + "/instance"
 
-	// Call OVH API to get instance data
 	err := client.Get(endpoint, &instances)
 	if err != nil {
 		return instances, err
 	}
 
-	// Return the billing price for this instance
 	return instances, nil
 }
 
 func GetCloudProjectInstance(client *ovh.Client, projectID string, instanceID string) (models.Instance, error) {
-	// Define the response structure for the instance billing
 	var instanceData models.Instance
 
-	// Build the API endpoint
 	endpoint := "/cloud/project/" + projectID + "/instance/" + instanceID
 
-	// Call OVH API to get instance data
 	err := client.Get(endpoint, &instanceData)
 	if err != nil {
 		return instanceData, err
 	}
 
-	// Return the billing price for this instance
 	return instanceData, nil
 }
